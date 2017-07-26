28b030755edf147710414ac889d71d94eb02c4ba
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-07-27 07:44:21 +0900

    Fix to load Phont file

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ from ctypes import *
 import wave
 import sys
 
+
+def load_phont_data(path):
+    with open(path, 'rb') as f:
+        return f.read()
+
 argc = len(sys.argv)
 if argc > 1:
     koe = sys.argv[1].encode('utf-8')
 else:
     koe = "ハローワールド".encode('utf-8')
 
+if argc > 2:
+    phont_data = load_phont_data(sys.argv[2])
+else:
+    phont_data = None
+
 i_speed = 100
 size = c_int(0)
-phont_data = 0
-
 wav = synthe_utf8(koe, i_speed, pointer(size), phont_data)
 if wav:
     with open('hello_world2.wav', 'wb') as wav_file:
